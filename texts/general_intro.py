28395f171b5ac71365c73007076189f8cc17916c
ErikMogensen <77505542+Ondonden@users.noreply.github.com>
2023-06-22 07:25:07 +0200

    Added general intro text

---
+++ texts/general_intro.py
@@ -0,0 +1,8 @@
+generel_intro = """WISC-V er en kognitiv test, som kan give et indblik i funktion indenfor en række områder, herunder det sproglige, det visuelt-rummelige, det logiske, arbejdshukommelsen og mental arbejdshastighed.
+WISC-testen undersøger disse områder på en struktureret og systematisk måde, og scores sammenlignes med børn på samme alder med samme biologiske køn.
+Testens resultater afhænger dog også af dagsformen, om man fx er meget træt eller nervøs og om man er motiveret for at deltage. 
+Det er vigtigt at huske på, at en sådan test er forbundet med en lille statistisk unøjagtighed.
+Derfor er testens resultat både angivet i en score, men også et 95% konfidens-interval, der siger noget om det generelle område vi med større statistisk sikkerhed kan sige at scores ligger indenfor.
+Testens resultat er opsummeret nedenfor.
+"""
+ 
