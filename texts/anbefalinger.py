2bd3bb2af14719521c2d114ad5dbc39fe1a9ec4b
ErikMogensen <77505542+Ondonden@users.noreply.github.com>
2023-06-23 13:04:34 +0200

    Updated recommendations

--- texts/anbefalinger.py
+++ texts/anbefalinger.py
@@ -2,10 +2,11 @@ import pandas as pd
 
 # anbefalinger, som afhænger af kognitivt niveau
 # Anbefalinger til lavt VFI:
-VFI_anbefaling_lav = '''Verbal forståelse indebærer at kunne forklare betydningen af ord, samt at kunne forklare abstrakte sammenhænge imellem ord.
-Når man scorer lavt på VFI, vil man ofte have svært ved det sproglige (både skriftligt og mundtligt) - samt at forstå og følge verbal instruktion.
-Derfor kan det hjælpe at supplere med visuel afbildning af strukturen i hverdagen og løsningen af opgaver (både ift. lektier og i undervisningen) fx form af piktogrammer, ugeskemaer. 
-'''
+VFI_anbefaling_lav = '''Verbal forståelse indebærer at kunne forklare betydningen af ord, ræsonnere om sproglige begreber, samt at kunne forklare tilegnet viden med ord.
+Når man scorer lavt på VFI, kan man ofte have svært ved disse ting både skriftligt og mundtligt - samt at forstå og følge verbal instruktion.
+Derfor kan det hjælpe at supplere instruktioner og beskeder med billeder, visualiseringer og gentagelser.
+Man kan også med fordel visualisere strukturen i hverdagen og løsningen af opgaver (både ift. lektier og i undervisningen) fx form af billeder, piktogrammer og ugeskemaer. 
+For at minimere elevens oplevelse af at være anderledes kan disse værktøjer fx anvendes i hele klassen, eller mere diskret på en computer.'''
 
 # Anbefalinger til lavt AHI:
 AHI_anbefaling_lav = '''Arbejdshukommelse er udtryk for hvor meget mental "plads" man har at arbejde på. 
@@ -26,9 +27,12 @@ Derfor anbefales det at man får længere tid til at løse opgaver - eller at de
 '''
 
 # Anbefalinger til lavt VSI:
-VSI_anbefaling_lav = '''
-Eksempel på anbefaling (VSI)
-'''
+VSI_anbefaling_lav = '''Det visuo-spatiale (visuelt-rummelige) indeks måler blandt andet evnen til at integrere forhold mellem helhed og del-elementer og evne til at rette sit fokus mod visuelle detaljer.
+Hvis man er testet med en lav score på VSI, kan det være hjælpsomt at skabe en meget tydelig visuel struktur - samt at understøtte denne visuelle struktur med en verbal gennemgang.
+Fx kan man ved at markere instruktioner med en farve (fx med overstregningstusch), for tydeligt at adskille svar fra spørgsmål.
+Denne strategi kan eleven også selv gøre brug af.
+Man kan også dække dele af ark med opgaver med papir, så eleven kun kan se den opgave de aktuelt arbejder med.
+Det kan desuden være konstruktivt at man ikke inkluderer visuelle indtryk på uddelte papirer, som ikke har direkte relevant for løsningen af opgaven - da det kan være svært at ignorere irrelevant visuel information.'''
 
 # Anbefalinger til lavt RSI:
 RSI_anbefaling_lav = '''
@@ -41,9 +45,10 @@ HIK_anbefaling_lav = ''''''
 generelle_anbefalinger = '''- Ros værdier I synes er vigtige, som er indenfor elevernes kontrol (fx flid, omtanke, ærlighed eller mod) fremfor resultater (fx fejl, hastighed, karakter).
 - Hvis det er vigtigt at pointere faglighed, så fokuserer på eleverne egen tidligere præstation, fremfor at sammenligninge med andre elever.
 - Vær opmærksomme på også at understøtte et lignende fokus blandt eleverne og sæt passende og rimelige rammer for at undgå evt. uhensigtsmæssig sammenlignings-kultur blandt eleverne.
-- Sørg for at gøre klassens rammer konkrete, tydelige (fx hvor længe de skal arbejde der er hvilke regler) og forudsigelige (ved at dage minder om hinanden, og at regler/beskeder/arbejdstid bliver overholdt).
+- Sørg for at gøre klassens rammer konkrete, tydelige (fx hvor længe de skal arbejde, og hvad der forventes i denne periode) og forudsigelige (fx gennem ugeskemaer, ved at dage/uger minder om hinanden, og at aftaler om opgaver/rammer/arbejdstid ikke ændres).
 - Sørg for at opgaver har en passende sværhedsgrad (den enkelte elev bør kunne løse størstedelen af opgaverne, størstedelen af tiden)
 - Hvis I oplever udfordringer med en elev, så undersøg om det kan forklares i konteksten (fx pga. forstyrrelser, hændelser inden eller efter undervisningen, for svære eller for lette opgaver). Overvej herefter om det kan forklares ved at elevens sociale, følelsesmæssige eller kognitive behov ikke bliver tilfredsstillet. 
+- Opfordr elever til at spørge hvis der er noget de ikke forstår, eller ikke ved hvad de skal.
 '''
 
 rækker = ['VFI', 'VSI', 'LRI', 'AHI', 'FHI', 'HIK']
--- texts/general_intro.py
+++ texts/general_intro.py
@@ -1,18 +1,21 @@
-generel_intro = """WISC-V er en kognitiv test, som kan give et indblik i funktion indenfor en række områder, herunder det sproglige, det visuelt-rummelige, det logiske, arbejdshukommelsen og mental arbejdshastighed.
+generel_intro = """WISC-V er en kognitiv test. Kognition er et andet ord for tænkning og testen kan give et indblik i mental funktion indenfor en række områder, herunder det sproglige, det visuelt-rummelige, det logiske, arbejdshukommelsen og mental arbejdshastighed.
 WISC-testen undersøger disse områder på en struktureret og systematisk måde, og scores sammenlignes med børn på samme alder med samme biologiske køn.
-Testens resultater afhænger dog også af dagsformen, om man fx er meget træt eller nervøs og om man er motiveret for at deltage. 
+Testens resultater afhænger udog også af dagsformen, om man fx er meget træt, nervøs eller om man er motiveret for at deltage. 
 Det er vigtigt at huske på, at en sådan test er forbundet med en lille statistisk unøjagtighed.
 Derfor er testens resultat både angivet i scores, men også med et 95% konfidens-interval (herefter forkortet 95%KI), der siger noget om det generelle område vi med større statistisk sikkerhed kan vurdere at scores ligger indenfor.
 Testens resultat er opsummeret nedenfor."""
+
 fortsat_intro_til_lav_begavelse = """En lav score på testen er ikke et udtryk for, at man ikke kan udvikle sig, men et udtryk for hvor man ligger ift. gennemsnittet.
 Et barn vil derfor oftest udvikle sig sammen med gennemsnittet - så barnets rå-score stiger samtidig med at gennemsnittet også stiger.
 Derfor ser man ofte, at børn bliver ved med at ligge i nærheden af det samme område ift. gennemsnittet, men det er vigtigt ikke at misforstå dette med fraværet af udvikling, eller manglende potentiale herfor.
-Man kan sammenligne det med, at være lavere end det gennemsnitlige barn i børnehaven.
-Her kan et barn 10 år senere også være blandt de laveste børn i udskolingen, selvom de er vokset i samme tempo som alle de andre. """
+Man kan sammenligne det med, at være højdemæssigt lavere end gennemsnittet i børnehaven.
+Her kan et barn 10 år senere også være blandt de laveste børn i udskolingen, selvom de er langt højere end de var i børnehaven.
+Med dette i tankerne er det dog vigtigt at støtte barnet i at finde værdier og mål, som relaterer sig til ting, som er indenfor barnets kontrol. 
+Eksempelvist er flid, ærlighed, omtanke, nysgerrighed og mod alt sammen fuldkommen indenfor elevens kontrol, hvor antallet af løste opgaver og ens karakterer kun delvist er indenfor elevens kontrol."""
 
 fortsat_intro_til_høj_begavelse = """En høj score på testen er ikke nødvendigvis et udtryk for at barnet ikke kan have behov for støtte.
-Høj begavelse kan også være et særligt behov, da man tit vil have et behov større faglige udfordringer, for at lære at koncentrere sig og gøre sig umage.
+Høj begavelse kan også være et særligt behov, da man tit vil have et behov for større faglige udfordringer, for at man også lærer at koncentrere sig og gøre sig umage.
 Høj begavelse kan desuden også medføre meget høje forventninger fra omgivelserne, som det kan være svært for barnet at leve op til.
-Med dette i tankerne er det vigtigt at støtte barnet i at finde værdier og mål, som relaterer sig til ting indenfor barnets kontrol. 
-Eksempelvist er flid, ærlighed og mod alt sammen indenfor elevens kontrol, fremfor antal af løste opgaver eller karakterer for opgaver, som kun delvist vil være indenfor elevens kontrol."""
+Med dette i tankerne er det vigtigt at støtte barnet i at finde værdier og mål, som relaterer sig til ting, som er indenfor barnets kontrol. 
+Eksempelvist er flid, ærlighed, omtanke og mod alt sammen fuldkommen indenfor elevens kontrol, hvor antallet af løste opgaver og ens karakterer kun delvist er indenfor elevens kontrol."""
  
