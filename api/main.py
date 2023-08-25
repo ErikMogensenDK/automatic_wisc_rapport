69b277dfc779bb8109d20faa3fbe10126adc6789
ErikMogensen <77505542+Ondonden@users.noreply.github.com>
2023-08-25 06:48:05 +0200

    Moved files into /api folder to enable vercel compatibility

--- api/main.py
+++ api/main.py
@@ -31,7 +31,7 @@ def run_python_function():
 
 
 if __name__ == '__main__':
-    print("Successfully received request!")
-    app.run()
+    #print("Successfully received request!")
+    #app.run()
     # if ran below, it will be available to everyone on network (i think)
-    #app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0')
